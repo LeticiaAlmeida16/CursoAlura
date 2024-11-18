d6c08db954886fe5c2f9ac17f3ebc918333add48
Letícia Nascimento de Almeida <124791668+LeticiaAlmeida16@users.noreply.github.com>
2024-11-18 19:23:56 -0300

    Add files via upload

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,81 @@
+// alert("Olá mundo, seja bem vindo ao jogo do número secreto!");
+// let numSecreto = 29;
+// console.log(numSecreto);
+// let chute = prompt("Escolha um número de 1 a 100")
+
+// if (numSecreto == chute) { 
+//     alert(`Voce descobriu o número secreto ${numSecreto} !`);
+// }
+// else {alert("Voce errou")};
+
+
+
+// alert('Boas vindas ao jogo do número secreto');
+
+// // Adicione um console.log para verificar o valor de "chute" após a entrada do usuário
+// let chute = prompt('Escolha um número entre 1 e 10');
+// console.log('Valor do chute:', chute);
+
+// let numeroSecreto = 4;
+
+// // Adicione um console.log para verificar a comparação entre "chute" e "numeroSecreto"
+// console.log('Resultado da comparação:', chute == numeroSecreto);
+
+// if (chute == numeroSecreto) {
+//     alert('Acertou');
+// } else {
+//     // Adicione um console.log para verificar o valor de "numeroSecreto" quando o jogador erra
+//     console.log('Valor do número secreto:', numeroSecreto);
+//     alert('O número secreto era ' + numeroSecreto);
+// }
+
+
+
+// alert("Boas vindas ao nosso site!");
+
+// // let nome = "LUA";
+// // let idade = 25;
+// numeroDeVendas = 50;
+// saldoDisponivel = 1000;
+// let mensagemDeErro = alert("Erro! Preencha todos os campos");
+
+// let nome = prompt("digite seu nome: ");
+// let idade = prompt("digite sua idade: ")
+
+// if (idade >= 18) { 
+//     alert("Voce pode tirar hab");
+// }
+// else (alert("Voce nn pode tirar hab"));
+
+
+
+// Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+// diaDaSemana = prompt('Qual é o dia da semana?');
+// if (diaDaSemana == 'Sábado') {
+//     alert('Bom fim de semana!');
+// } else if (diaDaSemana == 'Domingo') {
+//     alert('Bom fim de semana!');
+// } else {
+//     alert('Boa semana!');
+// }
+// Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+// numero = prompt('Digite um positivo ou negativo');
+// if (numero > 0) {
+//     alert('Número positivo!');
+// } else {
+//     alert('Número negativo!');
+// }
+// Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!" no console do navegador. Caso contrário, mostre "Tente novamente para ganhar.".
+// pontuacao = 105;
+// if (pontuacao >= 100) {
+//     console.log('Parabéns, você venceu!');
+// } else {
+//     console.log('Tente novamente para ganhar.');
+// }
+// Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+// let saldoConta = 500; // Exemplo de saldo
+// alert(`Seu saldo é de R$${saldoConta}.`);
+// Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.
+// let nome = prompt('Qual o seu nome?');
+// alert(`Boas vindas ${nome}`);
+
